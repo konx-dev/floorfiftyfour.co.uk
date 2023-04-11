a779d2648732af39cb6af1d583cd8cd616dea908
Oliver Cox <github@konx.dev>
2023-04-11 18:59:43 +0100

    tailwind config changes and sizing of book images

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,23 +15,15 @@ module.exports = {
         grey: '#D4D4D4',
         black: '#545454'
       },
-      black: {
-        default: '#000000',
-        80: 'rgba(0,0,0,0.8)',
-        60: 'rgba(0,0,0,0.6)'
-      },
-      white: {
-        default: '#FFFFFF',
-        80: 'rgba(255,255,255,0.8)',
-      },
-      grey: {
-        lighter: '#fafafa',
-        default: '#ddd',
-        darker: '#424242',
-      },
-      red: {
-        default: '#c01134',
-      },
+      white: '#ffffff',
+      'white-80': 'rgba(255,255,255,0.8)',
+      black: '#000000',
+      'black-80': 'rgba(0,0,0,0.8)',
+      'black-60': 'rgba(0,0,0,0.6)',
+      red: '#c01134',
+      grey: '#ddd',
+      'grey-lighter': '#fafafa',
+      'grey-darker': '#424242',
       debug: {
         red: 'red',
         blue: 'blue',
